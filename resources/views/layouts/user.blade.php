320785d81a0073127d2724d4032996f66a552904
george samy <gogosony124@gmail.com>
2024-05-17 18:23:23 +0400

    asdad

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -16,7 +16,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.c!--om">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700;800&family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+    <meta name="seobility" content="cb44f5118c9106cf2765d4571c49dc0e">
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">

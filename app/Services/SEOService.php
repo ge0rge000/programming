7820db0488456271ad09556a5e6d35673dca9df1
george samy <gogosony124@gmail.com>
2024-05-18 11:25:51 +0400

    sadad

--- app/Services/SEOService.php
+++ app/Services/SEOService.php
@@ -5,22 +5,23 @@
 namespace App\Services;
 
 use Spatie\SchemaOrg\Schema;
+use Illuminate\Support\Facades\URL;
 
 class SeoService
 {
     public function getSchemaData()
     {
+        $currentUrl = URL::current();
+
         $schemaData = [
             Schema::localBusiness()
                 ->name('MPTS TECH')
                 ->description('Specializes in creating websites and applications for iOS and Android, smart home installations, and IoT integration.')
-                ->url('https://mptstech.com/')  // Replace with your actual business URL
-                ,
+                ->url($currentUrl),
             Schema::organization()
                 ->name('MPTS TECH')
                 ->description('Expert in developing websites and mobile applications for iOS and Android, along with smart home installations and IoT integration.')
-                ->url('https://mptstech.com/')  // Replace with your actual business URL
-
+                ->url($currentUrl)
         ];
 
         return $schemaData;
